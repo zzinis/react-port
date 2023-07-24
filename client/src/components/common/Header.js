de67af3e4d3a42b2accd80a8a36e9d9c390d5c03
ja <85549857+zzinis@users.noreply.github.com>
2023-07-24 20:44:50 +0900

    header gnb,util 추가

--- client/src/components/common/Header.js
+++ client/src/components/common/Header.js
@@ -1,7 +1,28 @@
 import React from 'react';
 
 function Header() {
-	return <header>Header</header>;
+	return (
+		<>
+			<header>
+				<h1>Logo</h1>
+
+				<ul id='gnb'>
+					<li>Home</li>
+					<li>About</li>
+					<li>Shop</li>
+					<li>Notice</li>
+					<li>Gallery</li>
+					<li>Contact</li>
+				</ul>
+
+				<ul id='util'>
+					<li>login/Join</li>
+					<li>Mypage</li>
+					<li>Cart</li>
+				</ul>
+			</header>
+		</>
+	);
 }
 
 export default Header;

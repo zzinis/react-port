847cd99c06b4a4199c3355a357395bf6f6636e63
ja <85549857+zzinis@users.noreply.github.com>
2023-07-24 13:54:54 +0900

    App js components 연결

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,16 @@
-
 import './App.css';
+import Footer from './components/common/Footer';
+import Header from './components/common/Header';
+import Visual from './components/main/Visual';
 
 function App() {
-  return (
-    <>
-    </>
-  );
+	return (
+		<>
+			<Header />
+			<Visual />
+			<Footer />
+		</>
+	);
 }
 
 export default App;

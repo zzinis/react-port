910a3ecff9734020025d55e4d7d6d8bff315dc2c
zzinis <lizzyin@naver.com>
2023-07-29 15:51:28 +0900

    common layout 추가

---
+++ client/src/components/common/Layout.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Layout({ name }) {
+    return (
+        <>
+            <section className={`content ${name}`}>
+                <figure></figure>
+
+                <div className="inner">
+                    <h1>{name}</h1>
+                </div>
+
+
+            </section>
+        </>
+    )
+}
+
+export default Layout

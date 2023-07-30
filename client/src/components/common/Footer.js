a6e0e3116218caefefd8c7a0969798dd68bd99fc
ja <85549857+zzinis@users.noreply.github.com>
2023-07-30 19:01:29 +0900

    footer 임시 추가

--- client/src/components/common/Footer.js
+++ client/src/components/common/Footer.js
@@ -1,7 +1,24 @@
 import React from 'react';
 
 function Footer() {
-	return <footer>Footer</footer>;
+	return (
+		<footer>
+			<div class='footer-content'>
+				<p>&copy; 2023 Your Website. All rights reserved.</p>
+				<ul>
+					<li>
+						<a href='#'>Privacy Policy</a>
+					</li>
+					<li>
+						<a href='#'>Terms of Service</a>
+					</li>
+					<li>
+						<a href='#'>Contact Us</a>
+					</li>
+				</ul>
+			</div>
+		</footer>
+	);
 }
 
 export default Footer;

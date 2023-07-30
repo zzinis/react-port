6c6292802a7e47fe55bb4dee7acc0c9fd47dd170
ja <85549857+zzinis@users.noreply.github.com>
2023-07-30 18:39:14 +0900

    app.js routes 임시 수정

--- client/src/App.js
+++ client/src/App.js
@@ -2,17 +2,15 @@ import './App.css';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Footer from './components/common/Footer';
 import Header from './components/common/Header';
-import Visual from './components/main/Visual';
+import Department from './components/sub/Department';
 
 function App() {
 	return (
 		<>
 			<BrowserRouter>
+				<Header />
 				<Routes>
-					<Route path='/'>
-						<Header />
-						<Visual />
-					</Route>
+					<Route path='/department' element={Department}></Route>
 				</Routes>
 				<Footer />
 			</BrowserRouter>

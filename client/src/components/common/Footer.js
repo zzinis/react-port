8833cd222a49d26212f59fbad88b902f8dabeebb
zzinis <lizzyin@naver.com>
2023-08-02 09:59:27 +0900

    footer 레이아웃 수정

--- client/src/components/common/Footer.js
+++ client/src/components/common/Footer.js
@@ -1,24 +1,49 @@
-import React from 'react';
+import { faStar, faNewspaper, faCompactDisc, faMapMarkedAlt } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-function Footer() {
+const Footer = () => {
 	return (
-		<footer>
-			<div class='footer-content'>
-				<p>&copy; 2023 Your Website. All rights reserved.</p>
-				<ul>
-					<li>
-						<a href='#'>Privacy Policy</a>
-					</li>
-					<li>
-						<a href='#'>Terms of Service</a>
-					</li>
-					<li>
-						<a href='#'>Contact Us</a>
-					</li>
-				</ul>
+		<footer id="footer">
+			<div className="inner">
+				<h1>Contact us</h1>
+				<div className="wrap">
+					<div className="top">
+						<div className="first">
+							<h2>OUR INFORMATION</h2>
+							<p>Address<br /> (우)12345</p>
+							<address>서울특별시 강남구 강남대로 123<br />123, Gangnam-daero, Gangnam-gu, Seoul, Republic of Korea</address>
+						</div>
+						<div className="middle">
+							<h2>E-MAIL</h2>
+							<p>loremlorem1@naver.com</p>
+						</div>
+						<div className="last">
+							<h2>QUICK LINK</h2>
+							<ul>
+								<li>
+									<a href="#"><FontAwesomeIcon icon={faStar} /></a>
+									Department
+								</li>
+								<li>
+									<a href="#"><FontAwesomeIcon icon={faNewspaper} /></a>
+									Community
+								</li>
+								<li>
+									<a href="#"><FontAwesomeIcon icon={faCompactDisc} /></a>
+									Gallery
+								</li>
+								<li>
+									<a href="#"><FontAwesomeIcon icon={faMapMarkedAlt} /></a>
+									Location
+								</li>
+							</ul>
+						</div>
+					</div>
+					<div className="bottom">2021 DCODELAB &copy; copyright all right reserved.</div>
+				</div>
 			</div>
 		</footer>
 	);
-}
+};
 
 export default Footer;

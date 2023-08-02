7e44dec00de8407b31dd1916c81a5c07f33a8766
zzinis <lizzyin@naver.com>
2023-08-02 10:00:05 +0900

    main news component 추가

--- client/src/components/main/News.js
+++ client/src/components/main/News.js
@@ -2,7 +2,33 @@ import React from 'react'
 
 function News() {
     return (
-        <div>News</div>
+        <section id="news" className="myScroll">
+            <div className="inner">
+                <div className="wrap">
+                    <div className="left">
+                        <h1>ABOUT LOREM</h1>
+                        <p>
+                            Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime
+                            cum ex ipsum quo, autem hic tenetur delectus exercitationem eaque!
+                            Modi recusandae animi illo harum eum ab? Voluptatem velit ullam
+                            exercitationem, delectus sapiente sit veritatis quos enim odio
+                            deserunt recusandae quaerat provident molestias nobis unde.
+                            Expedita nemo repellendus reiciendis cum laudantium.
+                        </p>
+                    </div>
+                    <div className="right">
+                        <h2>
+                            MINIMAL
+                            <br />
+                            DESIGN
+                            <br />
+                            PUBLICATION
+                        </h2>
+                        <img src="img/pic7.jpg" alt="" />
+                    </div>
+                </div>
+            </div>
+        </section>
     )
 }
 

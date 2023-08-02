6a4e439c64363eb6ae9664da2fdf474cbb8ac821
zzinis <lizzyin@naver.com>
2023-08-02 10:02:38 +0900

    main.js popup introduce 연결

--- client/src/components/main/Main.js
+++ client/src/components/main/Main.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import Visual from './Visual';
 import News from './News';
+import Popup from './Popup';
+import Introduce from './Introduce';
 
 function Main() {
 	return (
 		<>
 			<Visual />
+			<Popup />
 			<News />
+			<Introduce />
 		</>
 	);
 }

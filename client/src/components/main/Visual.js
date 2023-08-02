86d27615acbf205955c24197aa1dbb1f495c533b
zzinis <lizzyin@naver.com>
2023-08-02 17:37:44 +0900

    visual slide 수정

--- client/src/components/main/Visual.js
+++ client/src/components/main/Visual.js
@@ -1,18 +1,28 @@
 import React, { useEffect, useState } from 'react';
 import { FaPlayCircle, FaChevronRight } from 'react-icons/fa';
 
-
 function Visual() {
-	// 슬라이드 이미지 다르게
-	const [leftSlide, setLeftSlide] = useState(0);
-	const [rightSlide, setRightSlide] = useState(0);
+	const [currentSlide, setCurrentSlide] = useState(0);
 	const leftImages = ['img/b1.jpg', 'img/b5.jpg', 'img/c4.jpg'];
 	const rightImages = ['img/b5.jpg', 'img/c4.jpg', 'img/b1.jpg'];
+	const leftTexts = [
+		'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit culpa cupiditate quo delectus iusto dolor',
+		'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur? lorem con sit amet',
+		'Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit culpa cupiditate quo dolor ipsum elit Nesciunt, ipsum '
+	];
+
+	const rightTexts = [
+		' Fugit culpa cupiditate quo Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt',
+		'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt, ipsum.',
+		'Lorem ipsum dolor sit amet consectetur adipisicing.t'
+	];
+
+	const sideboardTexts = ['Brinkk', 'Another', 'lorrem'];
 
 	const nextSlide = () => {
-		setLeftSlide((prevLeftSlide) => (prevLeftSlide + 1) % leftImages.length);
-		setRightSlide((prevRightSlide) => (prevRightSlide + 1) % rightImages.length);
+		setCurrentSlide((prevSlide) => (prevSlide + 1) % leftImages.length);
 	};
+
 	useEffect(() => {
 		// Automatically change slides every 5 seconds
 		const interval = setInterval(nextSlide, 5000);
@@ -21,41 +31,43 @@ function Visual() {
 			clearInterval(interval);
 		};
 	}, []);
+
 	return (
 		<figure id="visual">
 			<div className="inner">
 				<div className="wrap">
-					<div className="left">
+					<div className={`left ${currentSlide === 0 ? 'active' : ''}`}>
 						<div className="txt">
 							<svg width="280" height="130" viewBox="0 0 280 30">
 								<text>
-									Brinkk
-									<tspan dy="45" dx="-132">sideboard</tspan>
+									{sideboardTexts[currentSlide]}
+									<tspan dy="45" dx="-132">
+										sideboard
+									</tspan>
 								</text>
 							</svg>
-							{/* <h1>Brinkk<br>sideboard</h1> */}
 						</div>
-						<a href="/"><FaPlayCircle /></a>
+						<a href="/">
+							<FaPlayCircle />
+						</a>
 						<p>Video about product</p>
 						<div className="pic">
-							<img src={leftImages[leftSlide]} alt="" />
+							<img src={leftImages[currentSlide]} alt="" />
 						</div>
 					</div>
-					<div className="right">
+					<div className={`right ${currentSlide === 0 ? '' : 'active'}`}>
 						<h2>About</h2>
 						<div className="txtbox">
 							<article>
-								<p>
-									Lorem ipsum dolor sit amet consectetur adipisicing elit. Fugit culpa cupiditate quo<br /> delectus iusto dolor.
-								</p>
+								<p>{leftTexts[currentSlide]}</p>
 							</article>
 							<article>
-								<p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Nesciunt, ipsum.</p>
+								<p>{rightTexts[currentSlide]}</p>
 							</article>
 						</div>
-						<div className="num">{`0${rightSlide + 1}.`}</div>
+						<div className="num">{`0${currentSlide + 1}.`}</div>
 						<div className="pic">
-							<img src={rightImages[rightSlide]} alt="" />
+							<img src={rightImages[currentSlide]} alt="" />
 							<a href="#" className="prev"></a>
 							<a href="#" className="next" onClick={nextSlide}>
 								<FaChevronRight />
@@ -81,12 +93,13 @@ function Visual() {
 					<div className="second">
 						<p>designed</p>
 						<div className="line"></div>
-						<a href="#"><FaChevronRight /></a>
+						<a href="#">
+							<FaChevronRight />
+						</a>
 					</div>
 				</div>
 			</div>
 		</figure>
-
 	);
 }
 

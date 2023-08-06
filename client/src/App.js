f6f05bba48143fcebba30f4b1c7e2fa771deb386
zzinis <lizzyin@naver.com>
2023-08-06 20:13:42 +0900

    youtube,community,location app 연결

--- client/src/App.js
+++ client/src/App.js
@@ -3,10 +3,16 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Footer from './components/common/Footer';
 import Header from './components/common/Header';
 import Department from './components/sub/Department';
+import Youtube from './components/sub/Youtube';
+import Coummunity from './components/sub/Coummunity';
+
 import './scss/style.scss';
 import News from './components/main/News';
 import Visual from './components/main/Visual';
 import Main from './components/main/Main';
+import Location from './components/sub/Location';
+
+
 
 function App() {
 	return (
@@ -17,6 +23,9 @@ function App() {
 					<Route exact path='/' element={<Main />}>
 					</Route>
 					<Route path='/department' element={<Department />}></Route>
+					<Route path='/youtube' element={<Youtube />}></Route>
+					<Route path='/community' element={<Coummunity />}></Route>
+					<Route path='/location' element={<Location />}></Route>
 				</Routes>
 				<Footer />
 			</BrowserRouter>

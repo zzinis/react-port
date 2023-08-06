3c09a076a7734d70429b8841edb6df8b0c73fe77
zzinis <lizzyin@naver.com>
2023-08-06 20:11:56 +0900

    location.js 추가

---
+++ client/src/components/sub/Location.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Layout from '../common/Layout'
+
+function Location() {
+    return (
+        <Layout name={'Location'}>
+            <h1>Location</h1>
+            <p>Lorem, ipsum.</p>
+        </Layout>
+    )
+}
+
+export default Location

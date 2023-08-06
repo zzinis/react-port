59a8812746325486e54b9e635c912d2db91dd29a
zzinis <lizzyin@naver.com>
2023-08-06 20:12:59 +0900

    community.js 추가

--- client/src/components/sub/Coummunity.js
+++ client/src/components/sub/Coummunity.js
@@ -3,7 +3,10 @@ import Layout from '../common/Layout'
 
 
 function Coummunity() {
-    return <Layout name={'Community'}></Layout>
+    return <Layout name={'Community'}>
+        <h1>Community</h1>
+        <p>Lorem, ipsum.</p>
+    </Layout>
 }
 
 export default Coummunity

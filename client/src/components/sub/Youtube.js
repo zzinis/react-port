c6114f9354822785e3e62d31af3eafc77c947e7e
zzinis <lizzyin@naver.com>
2023-08-06 20:12:17 +0900

    youtube.js figure 수정

--- client/src/components/sub/Youtube.js
+++ client/src/components/sub/Youtube.js
@@ -2,7 +2,11 @@ import React from 'react'
 import Layout from '../common/Layout'
 
 function Youtube() {
-    return <Layout name={'Youtube'}>Youtube</Layout>
+    return <Layout name={'Youtube'}>
+        <h1>Youtube</h1>
+        <p>Lorem, ipsum.</p>
+
+    </Layout>
 }
 
 export default Youtube

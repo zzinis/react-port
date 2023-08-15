9ed7d02ae6921ea77aeb46368d046bd5f46baf4d
zzinis <lizzyin@naver.com>
2023-08-15 23:01:21 +0900

    Modal 컴포넌트 추가

---
+++ client/src/components/common/Modal.js
@@ -0,0 +1,10 @@
+const Modal = () => {
+    return (
+        <aside className='modal'>
+            <div className='con'></div>
+            <span className='close'>close</span>
+        </aside>
+    );
+};
+
+export default Modal;

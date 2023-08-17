c7eab711d84dad3276e00e99fc073e28975a8c29
zzinis <lizzyin@naver.com>
2023-08-17 23:22:57 +0900

    a태그 modal 연결

--- client/src/components/sub/Youtube.js
+++ client/src/components/sub/Youtube.js
@@ -29,7 +29,7 @@ function Youtube() {
                     {Vids.map((vid, idx) => {
                         return (
                             <article key={idx}>
-                                <a href="#" onClick={() => console.log(modal)}>
+                                <a href="#" onClick={() => modal.current.open()}>
                                     <img src={vid.snippet.thumbnails.standard.url} alt={vid.snippet.title} />
                                 </a>
                                 <div className="con">

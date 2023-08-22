4c774ae6ab1e0747d0b6cf1bd9ef24c54bde6346
zzinis <lizzyin@naver.com>
2023-08-22 22:18:43 +0900

    useEffect getpos btn 연결

--- client/src/components/main/Btns.js
+++ client/src/components/main/Btns.js
@@ -4,6 +4,20 @@ import { useRef, useEffect } from 'react';
 function Btns() {
     const btnRef = useRef(null);
     let pos = useRef([]);
+    const getPos = () => {
+        pos.current = [];
+        const secs = btnRef.current.parentElement.querySelectorAll('.myScroll');
+        for (const sec of secs) pos.current.push(sec.offsetTop);
+    };
+
+    useEffect(() => {
+        getPos();
+        window.addEventListener('resize', getPos);
+
+        return () => {
+            window.removeEventListener('resize', getPos);
+        };
+    }, []);
     return (
         <div>Btns</div>
     )

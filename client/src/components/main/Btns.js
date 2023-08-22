f186b487bd1cc18c6c17371721af2f632f7964e0
zzinis <lizzyin@naver.com>
2023-08-22 22:17:12 +0900

    btns useRef pos

--- client/src/components/main/Btns.js
+++ client/src/components/main/Btns.js
@@ -1,6 +1,9 @@
 import React from 'react'
+import { useRef, useEffect } from 'react';
 
 function Btns() {
+    const btnRef = useRef(null);
+    let pos = useRef([]);
     return (
         <div>Btns</div>
     )

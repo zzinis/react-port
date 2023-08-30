f8928a3ea238447a22abadb6fb92cb772c5f07c9
zzinis <lizzyin@naver.com>
2023-08-30 21:47:37 +0900

    location kakaomap

--- client/src/components/sub/Location.js
+++ client/src/components/sub/Location.js
@@ -2,6 +2,16 @@ import React from 'react'
 import Layout from '../common/Layout'
 
 function Location() {
+    const container = useRef(null);
+    const { kakao } = window;
+    const option = {
+        center: new kakao.maps.LatLng(33.450701, 126.570667),
+        level: 3,
+    };
+
+    useEffect(() => {
+        new kakao.maps.Map(container.current, option);
+    }, []);
     return (
         <Layout name={'Location'}>
             <h1>Location</h1>

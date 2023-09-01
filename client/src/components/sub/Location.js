1bf2539af4152d1be6410bfca4f9acf2b9dc7fa9
zzinis <lizzyin@naver.com>
2023-09-02 07:03:42 +0900

    map 3개

--- client/src/components/sub/Location.js
+++ client/src/components/sub/Location.js
@@ -6,11 +6,36 @@ function Location() {
     const container = useRef(null);
     const [Traffic, setTraffic] = useState(false);
     const [Location, setLocation] = useState(null);
+    // const { kakao } = window;
+    // const option = {
+    //     center: new kakao.maps.LatLng(33.450701, 126.570667),
+    //     level: 3,
+    // };
     const { kakao } = window;
-    const option = {
-        center: new kakao.maps.LatLng(33.450701, 126.570667),
-        level: 3,
-    };
+    const info = [
+        {
+            title: '강남역',
+            latlng: new kakao.maps.LatLng(37.51100661425726, 127.06162026853143),
+            imgSrc: `${process.env.PUBLIC_URL}/img/marker1.png`,
+            imgSize: new kakao.maps.Size(232, 99),
+            imgPos: { offset: new kakao.maps.Point(116, 99) },
+        },
+        {
+            title: '카카오 본사',
+            latlng: new kakao.maps.LatLng(37.40211707077346, 127.10344953763003),
+            imgSrc: `${process.env.PUBLIC_URL}/img/marker2.png`,
+            imgSize: new kakao.maps.Size(232, 99),
+            imgPos: { offset: new kakao.maps.Point(116, 99) },
+        },
+        {
+            title: '서울역',
+            latlng: new kakao.maps.LatLng(37.5662952, 126.9779451),
+            imgSrc: `${process.env.PUBLIC_URL}/img/marker3.png`,
+            imgSize: new kakao.maps.Size(232, 99),
+            imgPos: { offset: new kakao.maps.Point(116, 99) },
+        },
+    ];
+
 
     useEffect(() => {
         const mapInstance = new kakao.maps.Map(container.current, option);

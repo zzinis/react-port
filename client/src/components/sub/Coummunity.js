f82b036b9a90d986413bd87b249d593ce9a8a0c2
zzinis <lizzyin@naver.com>
2023-09-21 20:27:37 +0900

    커뮤니티 게시판 1

--- client/src/components/sub/Coummunity.js
+++ client/src/components/sub/Coummunity.js
@@ -1,8 +1,13 @@
 import React from 'react'
 import Layout from '../common/Layout'
+import { useRef, useState, useEffect } from 'react';
 
 
 function Coummunity() {
+
+    const input = useRef(null);
+    const textarea = useRef(null);
+    const [Posts, setPosts] = useState([]);
     return <Layout name={'Community'}>
         <h1>Community</h1>
         <p>Lorem, ipsum.</p>

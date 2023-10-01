21442b1ecc6b2f3fee4fb84f1f00722a089b2a73
zzinis <lizzyin@naver.com>
2023-10-01 19:56:53 +0900

    gallery axios usestate 연결

---
+++ client/src/components/sub/Gallery.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Layout from '../common/Layout';
+import axios from 'axios';
+import { useState, useEffect } from 'react';
+
+function Gallery() {
+    return (
+        <Layout name={'Gallery'}></Layout>
+    )
+}
+
+export default Gallery

6a96f49599a737d3e5ec796a9a06944ffe357322
zzinis <lizzyin@naver.com>
2023-10-01 19:57:43 +0900

    gallery flickr 연결

--- client/src/components/sub/Gallery.js
+++ client/src/components/sub/Gallery.js
@@ -4,6 +4,20 @@ import axios from 'axios';
 import { useState, useEffect } from 'react';
 
 function Gallery() {
+    const baseURL = 'https://www.flickr.com/services/rest/?format=json&nojsoncallback=1';
+    const key = '';
+    const method_interest = 'flickr.interestingness.getList';
+    const num = 20;
+    const url = `${baseURL}&api_key=${key}&method=${method_interest}&per_page=${num}`;
+
+    const [Items, setItems] = useState([]);
+
+    useEffect(() => {
+        axios.get(url).then((json) => {
+            console.log(json.data.photos.photo);
+            setItems(json.data.photos.photo);
+        });
+    }, []);
     return (
         <Layout name={'Gallery'}></Layout>
     )

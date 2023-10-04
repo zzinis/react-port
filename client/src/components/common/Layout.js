112619b9dc1aba48c526d713ffa79b07745de463
zzinis <lizzyin@naver.com>
2023-10-04 21:05:13 +0900

    layout split

--- client/src/components/common/Layout.js
+++ client/src/components/common/Layout.js
@@ -1,12 +1,22 @@
 import React, { useRef } from 'react';
 
-function Layout({ name, children }) {
+function Layout({ name, children, txt = 'Default' }) {
 	const frame = useRef(null);
 
 	return (
 		<section className={`content ${name}`} ref={frame}>
 			<figure>
 				<h1>{name}</h1>
+				<h2>
+					{txt.split('-').map((el, idx) => {
+						return (
+							<React.Fragment key={idx}>
+								{el}
+								<br />
+							</React.Fragment>
+						);
+					})}
+				</h2>
 			</figure>
 
 			<div className='inner'>

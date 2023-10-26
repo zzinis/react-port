bd85f1da72f68687a4aec33c540f482d5184056a
zzinis <lizzyin@naver.com>
2023-10-26 22:41:13 +0900

    Create Vids.js

---
+++ client/src/components/main/Vids.js
@@ -0,0 +1,27 @@
+import { memo } from 'react';
+import { useSelector } from 'react-redux';
+
+
+function Vids() {
+    const Vids = useSelector((store) => store.youtube.data);
+    console.log(Vids);
+    return (
+        <section id='vids' className='myScroll'>
+            {Vids.map((vid, idx) => {
+                if (idx >= 5) return null;
+
+
+                return (
+                    <article key={idx}>
+                        <div className='pic'>
+                            <img src={vid.snippet.thumbnails.standard.url} alt={vid.snippet.title} />
+                        </div>
+                    </article>
+                );
+            })}
+
+        </section>
+    );
+}
+
+export default memo(Vids);
